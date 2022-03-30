0ff7b43090f8d31f41128bd950019e108998f502
Kaj <kajbjorge@outlook.com>
2022-03-30 18:03:36 -0500

    created outline for 'view' object

---
+++ app.js
@@ -0,0 +1,18 @@
+/* 
+view: keeps the display updated (hits, misses, messages)
+model: keeps track of the abstract data (where the ships are, their status)
+controller: binds everything together (receives user input and implements game logic)
+*/
+
+//view: 
+ const view = {
+     displayMessage: function(msg) {
+
+     },
+     displayHit: function(location) {
+
+     },
+     displayMiss: function(location) {
+         
+     }
+ };
