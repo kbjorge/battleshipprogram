ab9918e1f7e240982b215340b0b17378b029f166
Kaj <kajbjorge@outlook.com>
2022-03-31 16:20:53 -0500

    finished model

--- app.js
+++ app.js
@@ -39,34 +39,54 @@ const ship3 = {
 */
 
 const model = {
-  
-    boardSize: 7,
-    numShips: 3,
-    shipsSunk: 0,
-    shipLength: 3,
-  
-    ships: [
+  boardSize: 7,
+  numShips: 3,
+  shipsSunk: 0,
+  shipLength: 3,
+
+  ships: [
     { locations: ["00", "07", "21"], hits: ["", "", ""] },
     { locations: ["37", "38", "39"], hits: ["", "", ""] },
     { locations: ["42", "43", "44"], hits: ["", "", ""] },
   ],
 
-    fire: function(guess) {
-        for (i = 0; i < this.numShips; i++) {
-            let ship = this.ships[i];
-            let locations = ship.locations;
-            let index = locations.indexOf(guess);
-            if (index >= 0) {
-                ship.hits[index] = "hit";
-                return true;
-            }
+  fire: function (guess) {
+    for (i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let locations = ship.locations; // using same variables = chaining - so we don't have to create a ton of new variables
+      let index = locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sunk my battleship!");
+          this.shipsSunk++;
         }
-        return false;
+      }
+      return true;
     }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
 
+  isSunk: function (ship) {
+    for (i = 0; i < this.shipLength; i++) {
+      if (ships[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
 };
 /*test:
 var shipper = ships[1];
 var spot = ship2.locations;
 console.log("Location is " + spot[1]);
+
+model.fire("00");
+model.fire("48");
 */
+
+
