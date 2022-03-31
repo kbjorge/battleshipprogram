8a3fb6cf1a54565a43a4c0acd1dca9ff568f57b5
Kaj <kajbjorge@outlook.com>
2022-03-30 23:21:50 -0500

    added ships

--- app.js
+++ app.js
@@ -7,12 +7,31 @@ controller: binds everything together (receives user input and implements game l
 //view: 
  const view = {
      displayMessage: function(msg) {
-
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
      },
      displayHit: function(location) {
-
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
      },
      displayMiss: function(location) {
-         
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
      }
  };
+
+ //model:
+ const ship1 = {
+     locations: ["00", "07", "21"],
+     hits: ["", "", ""]
+ };
+
+ const ship2 = {
+    locations: ["37", "38", "39"],
+    hits: ["", "", ""]
+};
+
+const ship3 = {
+    locations: ["42", "43", "44"],
+    hits: ["", "", ""]
+};
