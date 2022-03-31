f7575f10e4a3d991275dbf796ee6feb6c7f80c6c
Kaj <kajbjorge@outlook.com>
2022-03-31 14:03:02 -0500

    added model object

--- app.js
+++ app.js
@@ -4,34 +4,62 @@ model: keeps track of the abstract data (where the ships are, their status)
 controller: binds everything together (receives user input and implements game logic)
 */
 
-//view: 
- const view = {
-     displayMessage: function(msg) {
-        const messageArea = document.getElementById("messageArea");
-        messageArea.innerHTML = msg;
-     },
-     displayHit: function(location) {
-        const cell = document.getElementById(location);
-        cell.setAttribute("class", "hit");
-     },
-     displayMiss: function(location) {
-        const cell = document.getElementById(location);
-        cell.setAttribute("class", "miss");
-     }
- };
-view.displayHit("23");
- //model:
- const ship1 = {
-     locations: ["00", "07", "21"],
-     hits: ["", "", ""]
- };
+//view:
+const view = {
+  displayMessage: function (msg) {
+    const messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
 
- const ship2 = {
-    locations: ["37", "38", "39"],
-    hits: ["", "", ""]
+//model:
+/*   (if we did ships individually)
+const ship1 = {
+  locations: ["00", "07", "21"],
+  hits: ["", "", ""],
+};
+
+const ship2 = {
+  locations: ["37", "38", "39"],
+  hits: ["", "", ""],
 };
 
 const ship3 = {
-    locations: ["42", "43", "44"],
-    hits: ["", "", ""]
+  locations: ["42", "43", "44"],
+  hits: ["", "", ""],
 };
+*/
+
+const model = {
+  
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+  
+    ships: [
+    { locations: ["00", "07", "21"], hits: ["", "", ""] },
+    { locations: ["37", "38", "39"], hits: ["", "", ""] },
+    { locations: ["42", "43", "44"], hits: ["", "", ""] },
+  ],
+
+    fire: function(guess) {
+        for (i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+        }
+    }
+
+};
+/*test:
+var shipper = ships[1];
+var spot = ship2.locations;
+console.log("Location is " + spot[1]);
+*/
