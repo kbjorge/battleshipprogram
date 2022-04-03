071fb7b7454f8aca76a681824fe758d1c9c95414
Kaj <kajbjorge@outlook.com>
2022-04-03 13:05:06 -0500

    finished program

--- app.js
+++ app.js
@@ -45,9 +45,9 @@ var model = {
   shipLength: 3,
 
   ships: [
-    { locations: ["00", "01", "02"], hits: ["", "", ""] },
-    { locations: ["37", "38", "39"], hits: ["", "", ""] },
-    { locations: ["28", "35", "42"], hits: ["", "", ""] },
+    { locations: ["00", "00", "00"], hits: ["", "", ""] },
+    { locations: ["00", "00", "00"], hits: ["", "", ""] },
+    { locations: ["00", "00", "00"], hits: ["", "", ""] },
   ],
 
   
@@ -86,6 +86,56 @@ var model = {
     }
     return true;
   },
+
+  generateShipLocations: function() {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+        this.ships[i].locations = locations;
+    
+  }
+  console.log("Ships array: ");
+  console.log(this.ships);
+},
+
+generateShip: function() {
+  var direction = Math.floor(Math.random() * 2);
+  var row, col;
+
+  if (direction === 1) {
+    row = Math.floor(Math.random() * this.boardSize);
+    col = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+  } else {
+    row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+    col = Math.floor(Math.random() * this.boardSize);
+  }
+
+  var newShipLocations = [];
+  for (var i = 0; i < this.shipLength; i++) {
+    if (direction === 1) {
+      newShipLocations.push(row + "" + (col + i));
+    } else {
+      newShipLocations.push((row + i) + "" + col);
+    }
+  }
+  return newShipLocations;
+},
+
+collision: function(locations) {
+  for (var i = 0; i < this.numShips; i++) {
+    var ship = this.ships[i];
+    for (var j = 0; j < locations.length; j++) {
+      if (ship.locations.indexOf(locations[j]) >= 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
 };
 /*test:
 var shipper = ships[1];
@@ -150,5 +200,6 @@ window.onload = init;
 function init() {
     var fireButton = document.getElementById("fireButton");
     fireButton.onclick = handleFireButton;
+    model.generateShipLocations();
 }
 
